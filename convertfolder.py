b3c9487b9e00bf0767ff732f6c21a320405977e9
Brian Rice <brice@bigloops.com>
2020-02-02 17:48:42 -0800

    works now yay

--- convertfolder.py
+++ convertfolder.py
@@ -1,55 +1,67 @@
 #!/usr/local/bin/python3
 
 import sys
-import getopt
 import os
+import time
+import getopt
 import re
-from subprocess import run
+import subprocess
 
-def main(argv):
-    try:
-        opts, dirs_to_walk = getopt.getopt(argv,"nv")
-    except getopt.GetoptError:
-        print('convertfolder.py -v|-n dirs_to_walk')
-        sys.exit(2)
+for a in sys.argv:
+  for dirName, subdirList, fileList in os.walk(a, topdown=False):
+    if '.AppleDouble' in dirName:
+      continue
+    if '@SynoEAStream' in dirName:
+      continue
+    if '@SynoResource' in dirName:
+      continue
+    if '@eaDir' in dirName:
+      continue
+    for f in fileList:
+      if f.lower().endswith('.mp4'):
+        print("Skipping {} because no point in messing with mp4's".format(f))
+        continue
+      if not ( f.lower().endswith('.mpg') or
+               f.lower().endswith('.mpeg') or
+               f.lower().endswith('.wmv') or
+               f.lower().endswith('.rm') or
+               f.lower().endswith('.avi') or
+               f.lower().endswith('.rm') or
+               f.lower().endswith('.3g2') or
+               f.lower().endswith('.divx') or
+               f.lower().endswith('.flv')):
+        print("Skipping {} because not obviously a video file".format(f))
+        continue
+      if not os.access(os.path.join(dirName,f),os.R_OK):
+        print("Skipping {} because not readable".format(f))
+        continue
+      if os.stat(os.path.join(dirName,f)).st_size == 0:
+        print("Skipping {} because zero length".format(f))
+        continue
+      nf = f.encode('utf-8').decode('unicode_escape')
+      nf = re.sub(r'\\x..', r'', nf, count=0, flags=re.IGNORECASE)
+      nf = re.sub(r'\\x..', r'', nf, count=0, flags=re.IGNORECASE)
+      nf = re.sub(r' \[~of\d+\]', r'', nf, count=0, flags=re.IGNORECASE)
+      nf = re.sub(r' - File ~ of \d+', r'', nf, count=0, flags=re.IGNORECASE)
+      nf = re.sub(r' yEnc  ~ bytes  -', r'', nf, count=0, flags=re.IGNORECASE)
+      nf = re.sub(r'~', r'', nf, count=0, flags=re.IGNORECASE)
+      nf = re.sub(r'\(\)', r'', nf, count=0, flags=re.IGNORECASE)
+      nf = re.sub(r'\s', r'_', nf, count=0, flags=re.IGNORECASE)
+      nf = re.sub(r'&', r'_and_', nf, count=0, flags=re.IGNORECASE)
+      nf = re.sub(r'(.*)\..*',r'\1',nf) + ".mp4"
+      if (os.access(os.path.join(dirName,nf),os.F_OK) and
+         os.stat(os.path.join(dirName,nf)).st_size > 0):
+        print("Skipping {} because {} exists with nonzero length".format(f,nf))
+        continue
+      stat = os.stat(os.path.join(dirName,f))
+      print("gonna do ffmpeg -i {} {}".format(f,nf))
+      result = subprocess.run([ '/usr/local/bin/ffmpeg',
+                       '-i',
+                       os.path.join(dirName,f),
+                       os.path.join(dirName,nf) ])
+      if result.returncode:
+        print("ffmpeg -i {} {} appears to have failed".format(f,nf))
+      else:
+        os.rename(os.path.join(dirName,f),os.path.join(dirName,"deleteme-" + f))
+        os.utime(os.path.join(dirName,nf), times=(stat.st_mtime, stat.st_mtime))
 
-    basedir=os.getcwd()
-    print("basedir is {}".format(basedir))
-    for d in dirs_to_walk:
-        print("d is {}".format(d))
-        for root, dirs, files in os.walk(d):
-            if (".AppleDouble" in dirs):
-                dirs.remove(".AppleDouble")
-            print("root is {}".format(root))
-            for f in files:
-                if (f == ".DS_Store"):
-                    continue
-                if (re.match('deleteme-',f)):
-                    continue
-                ftype=re.sub(r'^.*\.(.*)$',r'\1',f)
-                if (ftype.lower() == 'mp4'):
-                    print("skipping {} because no point in messing with mp4s".format(f))
-                    continue
-                nname=re.sub(r'\s','_',f)
-                nname=re.sub(r'\'','',nname)
-                nname=re.sub(r'[\[\]~]','-',nname)
-                nname=re.sub(r'^(.*)\..*$',r'\1.mp4',nname)
-                print("{} --> {}".format(f,nname))
-                if os.path.isfile(os.path.join(basedir,d,nname)): 
-                    print("skipping {} because {} already exists".format(f,nname))
-                    continue
-                if ftype.lower() not in 'mov|mpeg|mpg|avi|wmv|rm|3g2|divx':
-                    print("skipping {} because type {} not recognized".format(f,ftype))
-                    continue
-                cp = run(['/usr/local/bin/ffmpeg','-i',os.path.join(basedir,d,f),os.path.join(basedir,d,nname)])
-                if cp.returncode:
-                    print("ffmpeg seems to have failed on {}".format(f))
-                else:
-                    new_atime = os.path.getatime(os.path.join(basedir,d,f))
-                    new_mtime = os.path.getmtime(os.path.join(basedir,d,f))
-                    os.utime(os.path.join(basedir,d,nname),(new_atime,new_mtime))
-                    os.rename(os.path.join(basedir,d,f),os.path.join(basedir,d,"deleteme-" + f))
-                    print("SUCCESS {}".format(f))
-
-if __name__ == "__main__":
-    main(sys.argv[1:])
